4a116fe6360ca39847fa163c1458dd7662486616
Brian Hicks <brian@brianthicks.com>
2022-02-08 06:27:20 -0600

    move grid generation into Maze

--- src/Main.elm
+++ src/Main.elm
@@ -1,41 +1,13 @@
 module Main exposing (..)
 
-import Graph exposing (Graph)
 import Html exposing (Html)
-import Maze
+import Maze exposing (Maze)
 import Random
 
 
-{-| IDs are laid out like:
-
-| 1 | 2 | 3 |
-| 4 | 5 | 6 |
-| 7 | 8 | 9 |
-
--}
-threeByThree : Graph ( Int, Int ) { present : Bool }
+threeByThree : Maze ( Int, Int ) { present : Bool }
 threeByThree =
-    Graph.empty
-        |> Graph.setNode 1 ( 0, 0 )
-        |> Graph.setNode 2 ( 0, 1 )
-        |> Graph.setNode 3 ( 0, 2 )
-        |> Graph.setNode 4 ( 1, 0 )
-        |> Graph.setNode 5 ( 1, 1 )
-        |> Graph.setNode 6 ( 1, 2 )
-        |> Graph.setNode 7 ( 2, 0 )
-        |> Graph.setNode 8 ( 2, 1 )
-        |> Graph.setNode 9 ( 2, 2 )
-        |> Graph.setEdge 1 2 { present = True }
-        |> Graph.setEdge 1 4 { present = True }
-        |> Graph.setEdge 2 3 { present = True }
-        |> Graph.setEdge 2 5 { present = True }
-        |> Graph.setEdge 3 6 { present = True }
-        |> Graph.setEdge 4 7 { present = True }
-        |> Graph.setEdge 4 5 { present = True }
-        |> Graph.setEdge 5 6 { present = True }
-        |> Graph.setEdge 5 8 { present = True }
-        |> Graph.setEdge 7 8 { present = True }
-        |> Graph.setEdge 8 9 { present = True }
+    Maze.squares identity { present = True } { width = 3, height = 3 }
 
 
 seed : Random.Seed
@@ -47,5 +19,5 @@ main : Html msg
 main =
     Html.main_ []
         [ Html.p [] [ Html.text (Debug.toString threeByThree) ]
-        , Html.p [] [ Html.text (Debug.toString (Maze.generate 1 9 (Maze.custom threeByThree) seed)) ]
+        , Html.p [] [ Html.text (Debug.toString (Maze.generate 1 9 threeByThree seed)) ]
         ]
--- src/Maze.elm
+++ src/Maze.elm
@@ -1,32 +1,74 @@
-module Maze exposing (Maze, custom, generate)
+module Maze exposing (Maze, custom, generate, squares)
 
 import Dict
 import Graph exposing (Graph)
 import Random exposing (Generator)
 
 
+squares : (( Int, Int ) -> node) -> { edge | present : Bool } -> { width : Int, height : Int } -> Maze node { edge | present : Bool }
+squares makeNode initEdge bounds =
+    let
+        width =
+            bounds.width - 1
+
+        height =
+            bounds.height - 1
+    in
+    -- make the coords and their IDs
+    List.range 0 height
+        |> List.concatMap
+            (\row ->
+                List.range 0 width
+                    |> List.map (\col -> ( col, row ))
+            )
+        |> List.indexedMap Tuple.pair
+        -- create a graph out of 'em!
+        |> List.foldl
+            (\( id, ( col, row ) ) graph ->
+                graph
+                    |> Graph.setNode id (makeNode ( col, row ))
+                    |> (if row + 1 <= height then
+                            Graph.setEdge id (id + width + 1) initEdge
+
+                        else
+                            identity
+                       )
+                    |> (if col + 1 <= width then
+                            Graph.setEdge id (id + 1) initEdge
+
+                        else
+                            identity
+                       )
+            )
+            Graph.empty
+        |> Squares
 
 
-
-custom : Graph node { edge | present : Bool } -> Maze node edge
+custom : Graph node { edge | present : Bool } -> Maze node { edge | present : Bool }
 custom =
     Maze
 
 
 type Maze node edge
-    = Maze (Graph node { edge | present : Bool })
+    = Maze (Graph node edge)
+    | Squares (Graph node edge)
 
 
-generate : Int -> Int -> Maze node edge -> Random.Seed -> Maze node edge
-generate start end (Maze graph) seed =
-    generateHelp [ start ] end graph seed
+generate : Int -> Int -> Maze node { edge | present : Bool } -> Random.Seed -> Maze node { edge | present : Bool }
+generate start end maze seed =
+    case maze of
+        Maze custom_ ->
+            Maze (generateHelp [ start ] end custom_ seed)
+
+        Squares squares_ ->
+            Squares (generateHelp [ start ] end squares_ seed)
 
 
-generateHelp : List Int -> Int -> Graph node { edge | present : Bool } -> Random.Seed -> Maze node edge
+generateHelp : List Int -> Int -> Graph node { edge | present : Bool } -> Random.Seed -> Graph node { edge | present : Bool }
 generateHelp stack end graph seed =
     case stack of
         [] ->
-            Maze graph
+            graph
 
         whereWeAre :: whereWeWere ->
             if whereWeAre == end then
